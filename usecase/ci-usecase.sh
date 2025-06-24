1673f726440bbf2c9ef5f357e96803b69b654706
shakeer405 <mohammed.ahmed4@ltimindtree.com>
2025-06-24 11:35:37 +0530

    Update ci-usecase.sh

--- usecase/ci-usecase.sh
+++ usecase/ci-usecase.sh
@@ -1,1 +1,17 @@
 #/bin/bash
+# Exit on any failure
+set -e
+
+# -------- CONFIGURATIONS --------
+PROJECT_NAME="my-java-app"
+DOCKER_IMAGE="myregistry.example.com/${PROJECT_NAME}:latest"
+SONAR_PROJECT_KEY="my-sonar-project"
+SONAR_HOST_URL="http://localhost:9000"
+SONAR_TOKEN="your-sonarqube-token"
+NEXUS_REPO_URL="http://nexus.example.com/repository/maven-releases/"
+NEXUS_USERNAME="your-nexus-username"
+NEXUS_PASSWORD="your-nexus-password"
+JFROG_REPO_URL="http://jfrog.example.com/artifactory/docker-local"
+JFROG_USERNAME="your-jfrog-username"
+JFROG_PASSWORD="your-jfrog-password"
+DOCKERFILE_PATH="."
